34cf878bfa1eb324390f0c3c5bd6a2901ea8e1ac
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-08-24 21:07:21 -0300

    feat: initialized web server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+///////////////////////
+// Files
 
 // Synchronous way
 
@@ -10,18 +14,29 @@ const fs = require("fs");
 // console.log("File has been written!");
 
 // Asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("ERROR! 💀");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR! 💀");
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written 😊");
-      });
-    });
-  });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written 😊");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+///////////////////////
+// SERVER
+
+const server = http.createServer((request, response) => {
+  response.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
